725ed4bd69e72d16ded4ab711b96d7aff8707a24
sulistio <fauzittio19@gmail.com>
2023-11-09 15:08:16 +0700

    study kasus author cyberLabs

---
+++ controllers/AuthorController.js
@@ -0,0 +1,132 @@
+const mysql = require('mysql2')
+const dbConfig = require('../config/database')
+const {
+    responseNotFound,
+    responseSuccess
+} = require('../traits/apiResponse')
+const pool = mysql.createPool(dbConfig)
+
+const getAuthors = (req, res) => {
+    const query = "SELECT * FROM author"
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query(query, (err, results) => {
+            if(err) throw err
+            
+            responseSuccess(res, results, 'author succesfully fetched')
+        })
+
+        connection.release()
+    })
+}
+
+const getAuthor =((req, res) => {
+    const id = req.params.id
+
+    const query = `SELECT * FROM author WHERE id=${id}`
+    
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query(query, (err, results) => {
+            if(err) throw err
+            
+            if(results.length== 0){
+                responseNotFound(res)
+                return
+            }
+
+            responseSuccess(res, results, 'author successfuly fetched')
+        })
+
+        connection.release()
+    })
+})
+
+const addAuthor = (req, res) => {
+    const data = {
+        nama: req.body.nama,
+        email: req.body.email,
+        alamat: req.body.alamat,
+        umur: req.body.umur,
+        media_sosial: req.body.media_sosial
+    }
+
+    const query = 'INSERT INTO author SET ?'
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query(query, [data], (err, results) => {
+            if(err) throw err
+
+            responseSuccess(res, results, 'author successfuly added')
+        })
+
+        connection.release()
+    })
+}
+
+const updateAuthor = ((req, res) => {
+    const id = req.params.id
+
+    const data = {
+        nama: req.body.nama,
+        email: req.body.email,
+        alamat: req.body.alamat,
+        umur: req.body.umur,
+        media_sosial: req.body.media_sosial
+    }
+
+    const query = `UPDATE author SET ? WHERE id=${id}`
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query(query, [data], (err, results) => {
+            if(err) throw err
+
+            if(results.affectedRows == 0){
+                responseNotFound(res)
+                return
+            }
+
+            responseSuccess(res, results, 'author successfuly update')
+        })
+
+        connection.release()
+    })
+})
+
+const deleteAuthor = (req, res) => {
+    const id = req.params.id
+
+    const query = `DELETE FROM author WHERE id=${id}`
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query(query, (err, results) => {
+            if(err) throw err
+
+            if(results.affectedRows == 0) {
+                responseNotFound(res)
+                return
+            }
+
+            responseSuccess(res, results, 'author successfuly delete')
+        })
+
+        connection.release()
+    })
+}
+
+module.exports = {
+    getAuthors,
+    getAuthor,
+    addAuthor,
+    updateAuthor,
+    deleteAuthor
+}
